f1ce712a2b5a08157fa87fecf8b1f3e4ade8f47f
JoseBSL <barragansljose@gmail.com>
2018-05-16 17:09:56 +1000

    1st commit R lesson

---
+++ swirl.R
@@ -0,0 +1,46 @@
+install.packages("rafalib")
+library(rafalib)
+
+install.packages("swirl")
+library(swirl)
+
+swirl()
+Jose
+1
+swirl()
+
+#### lesson 1 (basics)
+
+#### lesson 2 (paths, working directorys, workspace...)
+
+#getwd to obtain the path
+
+#ls() list of objects in your workspace
+
+#list.files()  list of files in your working directory. Also dir().
+
+#arg() To see the arguments of the funtion
+
+#file.rename(), first you write the file that you want to rename and then the new name.
+
+#create a copy of the file file.copy()
+
+#### lesson 3 (sequence of numbers)
+
+# colon operator :, which means these two dots. to ask about it ?`:` (use backticks)
+
+#seq also does the same job as colon operator, arguments, by and legth
+
+####lesson 4 (vectors)
+
+#logical operators == (equality), != (inequeality).
+
+#paste function
+
+#coercion numbers become characters to a R
+
+
+
+
+
+
