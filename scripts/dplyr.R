68415de501667a3c7fa5e2ba1bf2bcddb634ba83
JoseBSL <barragansljose@gmail.com>
2018-05-21 12:22:56 +1000

    last exam with different ways to unlist

--- scripts/dplyr.R
+++ scripts/dplyr.R
@@ -17,3 +17,34 @@ unlist(controls)
 
 controls<- filter(dat, Diet=="chow") %>% select(Bodyweight) %>% unlist
 
+install.packages("downloader")
+library(downloader)
+url="https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- basename(url)
+download(url,filename)
+
+#Creating foler and moving files to it
+#dir.create("data")
+file.rename("msleep_ggplot2.csv", "data/msleep_ggplot2.csv")
+file.rename("femaleControlsPopulation.csv", "data/femaleControlsPopulation.csv")
+file.rename("femaleMiceWeights.csv", "data/femaleMiceWeights.csv")
+file.rename("mice_pheno.csv", "data/mice_pheno.csv")
+
+
+dat<- read.csv("data/msleep_ggplot2.csv")
+View(dat)
+class(dat)
+#load library dplyr
+library(dplyr)
+?filter
+dat_primates<- filter(dat, order=="Primates")
+nrow(dat_primates)
+View(dat_primates)
+dat_primates_sleep<- select(dat_primates, sleep_total) %>% unlist
+
+class(dat_primates_sleep)
+mean(dat_primates$sleep_total)
+mean(dat_primates_sleep)
+?summarize
+#another different way to unlist
+dat_primates %>% summarise(mean= mean(sleep_total))
